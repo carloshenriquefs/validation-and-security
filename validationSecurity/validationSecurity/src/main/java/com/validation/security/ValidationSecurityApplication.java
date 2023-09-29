030e90a46dee96a0f16df98c80508c4a74ba0a78
Carlos Henrique <ichfs9108@gmail.com>
2023-09-28 23:53:57 -0300

    project created

---
+++ validationSecurity/validationSecurity/src/main/java/com/validation/security/ValidationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.validation.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationSecurityApplication.class, args);
+	}
+
+}
---
+++ validationSecurity/validationSecurity/src/test/java/com/validation/security/ValidationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validation.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
