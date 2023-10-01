431a4183a64b8da767b8419a795592d756345dc0
Carlos Henrique <ichfs9108@gmail.com>
2023-10-01 03:37:48 -0300

    Add services findAll - Cities and Event

--- validationSecurity/validationSecurity/src/main/java/com/validation/security/services/CityService.java
+++ validationSecurity/validationSecurity/src/main/java/com/validation/security/services/CityService.java
@@ -3,12 +3,18 @@ package com.validation.security.services;
 import com.validation.security.dto.CityDTO;
 import com.validation.security.entities.City;
 import com.validation.security.repositories.CityRepository;
+import com.validation.security.services.exceptions.DatabaseException;
 import com.validation.security.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.validation.security.constants.Constants.ENTITY_NOT_FOUND;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.validation.security.constants.Constants.*;
 
 @Service
 public class CityService {
@@ -17,9 +23,28 @@ public class CityService {
     private CityRepository cityRepository;
 
     @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> result = cityRepository.findAll();
+        List<CityDTO> dto = result.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public CityDTO findById(Long id) {
         City city = cityRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException(ENTITY_NOT_FOUND));
         return new CityDTO(city);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!cityRepository.existsById(id)) {
+            throw new ResourceNotFoundException(RESOURCE_NOT_FOUND);
+        }
+        try {
+            cityRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(FAIL_IN_REFERENTIAL_INTEGRITY);
+        }
     }
 }
--- validationSecurity/validationSecurity/src/main/java/com/validation/security/services/EventService.java
+++ validationSecurity/validationSecurity/src/main/java/com/validation/security/services/EventService.java
@@ -5,10 +5,17 @@ import com.validation.security.entities.Event;
 import com.validation.security.repositories.EventRepository;
 import com.validation.security.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.validation.security.constants.Constants.FAIL_IN_REFERENTIAL_INTEGRITY;
+import static com.validation.security.constants.Constants.RESOURCE_NOT_FOUND;
 
 @Service
 public class EventService {
@@ -17,9 +24,28 @@ public class EventService {
     private EventRepository eventRepository;
 
     @Transactional(readOnly = true)
+    public List<EventDTO> findAll() {
+        List<Event> result = eventRepository.findAll();
+        List<EventDTO> dto = result.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public EventDTO findById(Long id) {
         Optional<Event> obj = eventRepository.findById(id);
         Event entity = obj.orElseThrow(() -> new ResourceNotFoundException(""));
         return new EventDTO(entity);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void deleteById(Long id) {
+        if (!eventRepository.existsById(id)) {
+            throw new ResourceNotFoundException(RESOURCE_NOT_FOUND);
+        }
+        try {
+            eventRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResourceNotFoundException(FAIL_IN_REFERENTIAL_INTEGRITY);
+        }
     }
 }
