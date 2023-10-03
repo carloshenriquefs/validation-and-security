91a62cf867947ba655f60afb9a03c957be435154
Carlos Henrique <ichfs9108@gmail.com>
2023-10-03 01:05:49 -0300

    Fix Login

---
+++ validationSecurity/validationSecurity/src/main/java/com/validation/security/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.validation.security.repositories;
+
+
+import com.validation.security.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
